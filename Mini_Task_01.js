59bc3150d828ff91c343615077f6c9a03421709d
JAYASHAV <jayashavgupta@gmail.com>
2025-08-24 13:27:41 +0530

    Motu Patlu Mini Task 01

---
+++ Mini_Task_01.js
@@ -0,0 +1,48 @@
+let studend_detail_1 = {
+    name: "MOTU" ,
+    branch: "AI&DS" ,
+    id: "1234XX5678",
+    semester:"5th",
+    collage_name: "APNA COLLAGE",
+}
+let studend_detail_2 = {
+    name: "PATLU" ,
+    branch: "AI&DS" ,
+    id: "1234XX5765",
+    semester:"5th",
+    collage_name: "APNA COLLAGE",
+}
+
+console.log("<-- SAMADHAN 2.0 -->")
+console.log("STUDENT DETAILS :")
+console.log("* Student 01 :")
+console.log("NAME    : ", studend_detail_1.name)
+console.log("ID      : ", studend_detail_1.id)
+console.log("BRANCH  : ", studend_detail_1.branch)
+console.log("SEM     : ",studend_detail_1.semester)
+console.log("COLLAGE : ",studend_detail_1.collage_name)
+console.log("* Student 02 :")
+console.log("NAME    : ", studend_detail_2.name)
+console.log("ID      : ", studend_detail_2.id)
+console.log("BRANCH  : ", studend_detail_2.branch)
+console.log("SEM     : ",studend_detail_2.semester)
+console.log("COLLAGE : ",studend_detail_2.collage_name)
+
+
+// OUTPUT:
+/* 
+<-- SAMADHAN 2.0 -->
+STUDENT DETAILS :
+* Student 01 :
+NAME    :  MOTU
+ID      :  1234XX5678
+BRANCH  :  AI&DS
+SEM     :  5th
+COLLAGE :  APNA COLLAGE
+* Student 02 :
+NAME    :  PATLU
+ID      :  1234XX5765
+BRANCH  :  AI&DS
+SEM     :  5th
+COLLAGE :  APNA COLLAGE
+*/
