fb52946f3bc784afc05c55763b89d9efc813bb39
JAYASHAV <jayashavgupta@gmail.com>
2025-08-25 23:04:14 +0530

    Motu Patlu Mini Task 05

---
+++ Mini_Task_05/frontend.js
@@ -0,0 +1,5 @@
+fetch("http:localhost:3000/",{
+    method: "POST",
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({name: "GASITA RAM"})
+})
---
+++ Mini_Task_05/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+let student=["MOTU","PATLU"]
+let list = {
+    name:student
+}
+
+app.get('/', (req, res) => {
+  res.json(list.name)
+})
+app.post('/', (req, res) => {
+    let new_student = req.body.name
+    console.log(new_student)
+    student.push(new_student)
+    console.log(student)
+  res.json(list.name)
+})
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
+
