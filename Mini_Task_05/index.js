91785198ddb4f0997d25882659e64bbf8bd8a9c0
JAYASHAV <jayashavgupta@gmail.com>
2025-08-28 23:59:00 +0530

    Motu Patlu Mini_Task_05

--- Mini_Task_05/index.js
+++ Mini_Task_05/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from "cors"
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(express.json())
 
-let student=["MOTU","PATLU"]
+let student=["MOTU","PATLU", "JHATKA", "GHASETARAM", "CHINGUM", "BOXER"]
 let list = {
     name:student
 }
